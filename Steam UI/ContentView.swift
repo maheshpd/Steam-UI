9f30bd2cdb12699755953c82909fe2b5fecfa756
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-10-12 17:24:58 +0530

    Initial Commit

---
+++ Steam UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Steam UI
+//
+//  Created by Mahesh Prasad on 12/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Steam UI/Steam_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steam_UIApp.swift
+//  Steam UI
+//
+//  Created by Mahesh Prasad on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Steam_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
